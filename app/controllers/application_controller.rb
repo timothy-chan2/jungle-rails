e1116368391abafe709a19a0b5706d29e8bd9b99
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-01-07 01:02:49 +0000

    Add logic to control what happens when user is logged in or not

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def current_user
+    # If user is logged in, the user object is saved to @current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def authorize
+    # When user is not logged in, they get sent to the login page
+    redirect_to '/login' unless current_user
+  end
+
   private
 
   def cart
