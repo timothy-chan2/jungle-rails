0eec1a80ff2e5725c2a4ac663f8b449ef134c3c5
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-01-10 23:37:28 +0000

    Add validation checks for information entered when user is registering

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
+  
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+
 end
