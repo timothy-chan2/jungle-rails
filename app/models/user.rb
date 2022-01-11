e978aa9441f3d3b294ff610a97384dfec91d4711
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-01-11 01:47:01 +0000

    Add validation for password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates :password, confirmation: true
+  validates :password, confirmation: true, length: { minimum: 4 }
   validates :password_confirmation, presence: true
 
 end
