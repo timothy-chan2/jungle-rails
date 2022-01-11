9d64299a0389a390c5c72eb008eee26ce1840694
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-01-11 04:20:11 +0000

    Move the user authentication logic into class method authenticate_with_credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,11 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_by_email(params[:email])
-    # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:password])
+    # Delete these 4 lines once the authenticate_with_credentials method passes all it's test cases
+    # user = User.find_by_email(params[:email])
+    # # If the user exists AND the password entered is correct.
+    # if user && user.authenticate(params[:password])
+
+    if user = User.authenticate_with_credentials(params[:email], params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user 
       # logged in when they navigate around our website.
       session[:user_id] = user.id
