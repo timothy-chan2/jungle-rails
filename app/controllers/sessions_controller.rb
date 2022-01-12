f23f3635e230391c403fd4583c5e156debeb4bb4
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-01-12 04:15:11 +0000

    Remove commented out code that is not needed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,5 @@
 class SessionsController < ApplicationController
   def create
-    # Delete these 4 lines once the authenticate_with_credentials method passes all it's test cases
-    # user = User.find_by_email(params[:email])
-    # # If the user exists AND the password entered is correct.
-    # if user && user.authenticate(params[:password])
-
     if user = User.authenticate_with_credentials(params[:email], params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user 
       # logged in when they navigate around our website.
